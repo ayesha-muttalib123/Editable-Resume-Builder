16c9f2cf6f19748cc14462de4739222246d41e7e
ayesha-muttalib123 <ayeshamutallib007@gmail.com>
2024-11-05 02:41:44 +0500

    changes in app.ts

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ form.addEventListener('submit', function (event) {
     var work = document.getElementById('work').value;
     var skills = document.getElementById('skills').value;
     // Generate the resume display
-    var resumeHtml = "\n            <h2>".concat(name, "</h2>\n            <p><strong>Email:</strong> ").concat(email, "</p>\n            <p><strong>Phone:</strong> ").concat(phone, "</p>\n\n            <div class=\"resume-section\">\n                <h3>Education</h3>\n                <p>").concat(education, "</p>\n            </div>\n\n            <div class=\"resume-section\">\n                <h3>Work Experience</h3>\n                <p>").concat(work, "</p>\n            </div>\n\n            <div class=\"resume-section\">\n                <h3>Skills</h3>\n                <p>").concat(skills, "</p>\n            </div>\n        ");
+    var resumeHtml = "\n            <h2>".concat(name, "</h2>\n            <p><strong><span contenteditable=\"true\" >Email:</strong> ").concat(email, "</span></p>\n            <p><span contenteditable=\"true\" ><strong>Phone:</strong> ").concat(phone, "</span></p>\n\n            <div class=\"resume-section\">\n                <h3>Education</h3>\n                <p contenteditable=\"true\">").concat(education, "</p>\n            </div>\n\n            <div class=\"resume-section\">\n                <h3>Work Experience</h3>\n                <p contenteditable=\"true\">").concat(work, "</p>\n            </div>\n\n            <div class=\"resume-section\">\n                <h3>Skills</h3>\n                <p contenteditable=\"true\">").concat(skills, "</p>\n            </div>\n        ");
     if (resumeDisplay) {
         resumeDisplay.innerHTML = resumeHtml;
     }
